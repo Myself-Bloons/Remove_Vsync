0f55ba61ac31984a8a9cf752c6ee024ceb1163f0
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2025-11-02 20:21:01 +0100

    Add files via upload

---
+++ GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Il2CppAssets.Scripts.Models;
+global using Il2CppAssets.Scripts.Models.Towers;
+global using Il2CppAssets.Scripts.Models.Towers.Behaviors;
+global using Il2CppAssets.Scripts.Models.Towers.Behaviors.Attack;
+global using Il2CppAssets.Scripts.Simulation.Towers;
+global using Il2CppAssets.Scripts.Unity;
+global using BTD_Mod_Helper.Api.ModOptions;
+global using BTD_Mod_Helper.Extensions;
---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+namespace remove_vsync;
+
+public static class ModHelperData
+{
+    public const string Name = "Remove VSync";
+    public const string Description = "Removes vertical sync and sets target frame rate to 244 or the one you set in the menu FPS for smoother gameplay. Requires to restart the game.";
+    public const string Version = "1.0.0";
+    public const string RepoOwner = "Myself-Bloons";
+    public const string RepoName = "Remove-Vsync";
+    public const string Author = "Myself";
+    public const string WorksOnVersion = "51.2";
+    public const string DllName = "remove_vsync.dll";
+    public const string Icon = "Icon.png";
+}
